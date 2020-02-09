55c92c092af94b2b898ed9a5c00b43644f5e487f
ewagner <ericwagnerpreston@gmail.com>
2020-02-09 16:21:24 -0600

    Initial Commit - Create .net core SignalR sample app and add a CountDown() method

---
+++ ChatDemoR/ChatDemoR/Hubs/Chathub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatDemoR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task CountDown()
+        {
+            var numbers = Enumerable.Range(0, 10).Select(x => x);
+            foreach(var num in numbers)
+            {
+                await Task.Delay(1200);
+                await Clients.All.SendAsync("CountDownReceived", num.ToString());  
+            }
+        }
+    }
+}
