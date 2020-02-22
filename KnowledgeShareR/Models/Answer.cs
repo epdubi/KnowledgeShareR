8c94a49d4e47ed8d6c5d9b8ae10d8241b1d5ac29
ewagner <ericwagnerpreston@gmail.com>
2020-02-22 10:23:01 -0600

    Clean up using statements

--- KnowledgeShareR/Models/Answer.cs
+++ KnowledgeShareR/Models/Answer.cs
@@ -1,9 +1,3 @@
-using Microsoft.AspNetCore.SignalR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace KnowledgeShareR.Models
 {
     public class Answer
--- KnowledgeShareR/Models/Question.cs
+++ KnowledgeShareR/Models/Question.cs
@@ -1,8 +1,4 @@
-using Microsoft.AspNetCore.SignalR;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace KnowledgeShareR.Models
 {
