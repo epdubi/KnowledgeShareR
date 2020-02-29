0ae3173bc610b5e6c1cc937058e388928a23d31b
ewagner <ericwagnerpreston@gmail.com>
2020-02-29 08:11:17 -0600

    Add INSERT scripts

---
+++ SqlScripts/InsertQuestionAnswers.sql
@@ -0,0 +1,18 @@
+SELECT * FROM Questions q 
+JOIN Answers a ON q.QuestionId = a.QuestionId
+JOIN Votes v ON q.QuestionId = v.QuestionId
+
+INSERT INTO Questions(Text)
+VALUES('What is your favorite color?')
+
+INSERT INTO Answers(QuestionId, Text, IsCorrect)
+VALUES(1, 'Blue', 0)
+
+INSERT INTO Answers(QuestionId, Text, IsCorrect)
+VALUES(1, 'Green', 1)
+
+INSERT INTO Answers(QuestionId, Text, IsCorrect)
+VALUES(1, 'Pink', 0)
+
+INSERT INTO Answers(QuestionId, Text, IsCorrect)
+VALUES(1, 'Orange', 0)
